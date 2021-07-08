847fafd3ac39c24a11cb3dfb1644db9e0a57f208
jaej <jaej.dev@gmail.com>
2021-07-09 00:52:35 +0900

    fixed gen_tags.sh

--- cpp/tesla/gen_tags.sh
+++ cpp/tesla/gen_tags.sh
@@ -5,6 +5,7 @@ rm cscope.files
 rm tags
 
 find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' \) -print > cscope.files
+cscope -i cscope.files
 
 ctags -R -L cscope.files
 echo "Completed to create tags file..."
